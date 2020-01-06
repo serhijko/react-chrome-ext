3f11aed650c25b4f5d4a8a1df147236b144721e3
Serhij Korneluk <serhij@debian.home>
2020-01-06 17:21:46 +0300

    seconde commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  React.useEffect(() => {
+    // Register the message listener on load
+    // eslint-disable-next-line
+    chrome.runtime.onMessage.addListener((action) => {
+      switch (action.type) {
+        case 'filter-by-duration': {
+          console.log('received click event from context menu')
+          break
+        }
+        default:
+          break
+      }
+    })
+  }, [])
+
+  return null
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.createElement('div')
+rootEl.id = 'react-chrome-ext'
+document.body.appendChild(rootEl)
+
+ReactDOM.render(<App />, rootEl)
